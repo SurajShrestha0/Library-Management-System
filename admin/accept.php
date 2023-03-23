72c013ea9022e75edd964ecd6edc7571a48dfbd9
Naresh Sejwal <107300202+Naresh132@users.noreply.github.com>
2023-03-23 23:53:20 +0545

    Add files via upload
    admin accept and renewal the iteams

---
+++ admin/accept.php
@@ -0,0 +1,65 @@
+<?php
+require('dbconn.php');
+//Retrieve the value of the "id1" parameter from the URL query string and assign variable 
+$bookid=$_GET['id1'];
+//Retrieve the value of the "id2" parameter from the URL query string and assign variable 
+$rollno=$_GET['id2'];
+//select category
+$sql="select Category from LMS.user where RollNo='$rollno'";
+// Execute the SQL query using the connection object and store the result
+$result=$conn->query($sql);
+//Fetch a single row of data from the query result as an associative array
+$row=$result->fetch_assoc();
+//Extract the value of the "Category" column from the fetched row and assign it to a variable named $category
+$category=$row['Category'];
+if($category == 'GEN' || $category == 'OBC' )
+//update record id, rollno
+{$sql1="update LMS.record set Date_of_Issue=curdate(),Due_Date=date_add(curdate(),interval 60 day),Renewals_left=1 where BookId='$bookid' and RollNo='$rollno'";
+// condtion checking the aql query
+if($conn->query($sql1) === TRUE)
+//update book tables
+{$sql3="update LMS.book set Availability=Availability-1 where BookId='$bookid'";
+// Execute the SQL query using the connection object and store the result
+ $result=$conn->query($sql3);
+ //inset the value in message table
+ $sql5="insert into LMS.message (RollNo,Msg,Date,Time) values ('$rollno','Your request for issue of BookId: $bookid  has been accepted',curdate(),curtime())";
+ //database query connection
+ $result=$conn->query($sql5);
+echo "<script type='text/javascript'>alert('Success')</script>";
+// Redirect the user to the "issue_requests.php" page after a 1-second delay
+header( "Refresh:0.01; url=issue_requests.php", true, 303);
+}
+else
+{
+    //error message
+	echo "<script type='text/javascript'>alert('Error')</script>";
+    // Redirect the user to the "issue_requests.php" page after a 1-second delay
+    header( "Refresh:1; url=issue_requests.php", true, 303);
+
+}
+}
+else
+//upadte the record table
+{$sql2="update LMS.record set Date_of_Issue=curdate(),Due_Date=date_add(curdate(),interval 180 day),Renewals_left=1 where BookId='$bookid' and RollNo='$rollno'";
+// condition conncetion 
+if($conn->query($sql2) === TRUE)
+//update the book table
+{$sql4="update LMS.book set Availability=Availability-1 where BookId='$bookid'";
+ $result=$conn->query($sql4);
+ //inset the value in message table
+ $sql6="insert into LMS.message (RollNo,Msg,Date,Time) values ('$rollno','Your request for issue of BookId: $bookid has been accepted',curdate(),curtime())";
+ $result=$conn->query($sql6);
+ // alert message box  for sucessful
+echo "<script type='text/javascript'>alert('Success')</script>";
+// Redirect the user to the "issue_requests.php" page after a 1-second delay
+header( "Refresh:1; url=issue_requests.php", true, 303);
+}
+else
+{
+    //aleart message Fail (Error)
+	echo "<script type='text/javascript'>alert('Error')</script>";
+    // Redirect the user to the "issue_requests.php" page after a 1-second delay
+    header( "Refresh:1; url=issue_requests.php", true, 303);
+}
+}
+?>
---
+++ admin/acceptrenewal.php
@@ -0,0 +1,64 @@
+<?php
+//include database connection  file
+require('dbconn.php');
+// Retrieve the values of the "id1" and "id2" parameters from the URL query string and assign them to variables
+$bookid = $_GET['id1'];
+$bookid=$_GET['id1'];
+$rollno=$_GET['id2'];
+// Retrieve the user's category from the database
+$sql="select Category from LMS.user where RollNo='$rollno'";
+$result=$conn->query($sql);
+$row=$result->fetch_assoc();
+$category=$row['Category'];
+// Update the due date and renewals left for the book record based on the user's category
+if($category == 'GEN' || $category == 'OBC' )
+{$sql1="update LMS.record set Due_Date=date_add(Due_Date,interval 60 day),Renewals_left=0 where BookId='$bookid' and RollNo='$rollno'";
+ 
+if($conn->query($sql1) === TRUE)
+{
+ // Delete the renewal request from the database
+ $sql3="delete from LMS.renew where BookId='$bookid' and RollNo='$rollno'";
+ // Execute the SQL query using the connection object and store the result
+ $result=$conn->query($sql3);
+ //sql query for insert the value in data
+ $sql5="insert into LMS.message (RollNo,Msg,Date,Time) values ('$rollno','Your request for renewal of BookId: $bookid  has been accepted',curdate(),curtime())";
+//Execute the SQL query
+ $result=$conn->query($sql5);
+ // Display the error message
+echo "<script type='text/javascript'>alert('Success')</script>";
+//refresh page after 0.01 seconds and redirect to renew_requests.php         
+header( "Refresh:0.01; url=renew_requests.php", true, 303);
+}
+else
+{
+	echo "<script type='text/javascript'>alert('Error')</script>";
+    header( "Refresh:0.01; url=renew_requests.php", true, 303);
+}
+}
+else
+//sql query for update record database table
+{$sql2="update LMS.record set Due_Date=date_add(Due_Date,interval 180 day),Renewals_left=0 where BookId='$bookid' and RollNo='$rollno'";
+//Execute the SQL query and check if it was successful
+if($conn->query($sql2) === TRUE)
+//SQL query to delete record from database table
+{$sql4="delete from LMS.renew where BookId='$bookid' and RollNo='$rollno'";
+// Execute the SQL query using the connection object and store the result
+ $result=$conn->query($sql4);
+//insert the data value into message databse table
+ $sql6="insert into LMS.message (RollNo,Msg,Date,Time) values ('$rollno','Your request for renewal of BookId: $bookid has been accepted',curdate(),curtime())";
+// Execute the SQL query using the connection object and store the result
+ $result=$conn->query($sql6);
+ //display the  success message 
+echo "<script type='text/javascript'>alert('Success')</script>";
+// Redirect the user to the "renew_requests.php" page after a 1-second delay
+header( "Refresh:0.01; url=renew_requests.php", true, 303);
+}
+else
+{ 
+    //display the error message (alart box)
+	echo "<script type='text/javascript'>alert('Error')</script>";
+    // Redirect the user to the "renew_requests.php" page after a 1-second delay
+    header( "Refresh:0.01; url=renew_requests.php", true, 303);
+}
+}
+?>
