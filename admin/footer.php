ba9bcc7cf5a5a67ac261fa7a1395af4383272950
Naresh Sejwal <107300202+Naresh132@users.noreply.github.com>
2023-04-28 23:34:43 +0545

    Footer part

---
+++ admin/footer.php
@@ -0,0 +1,21 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+<footer class="bg-dark text-center text-black" style="position: fixed; bottom: 0; width: 100%; text-align: center; color: #333333;left: 10px;;">
+  <div class="container p-4">
+    <!--Social media link -->
+    <section class="mb-4">
+      <!--Facebook -->
+      <a class="btn btn-outline-light btn-floating m-1" style="color: white;" href="https://www.facebook.com/gops.bhattarai.7" role="button" target="_blank"><i class="fab fa-facebook-f"></i></a>
+      <!--Instgram -->
+      <a class="btn btn-outline-light btn-floating m-1" style="color: white;" href="https://www.instagram.com/sooraj__stha/" role="button" target="_blank"><i class="fab fa-instagram"></i></a>
+      <!--linkedin text -->
+      <a class="btn btn-outline-light btn-floating m-1" style="color: white;" href="https://www.linkedin.com/in/naresh-sejwal-381303239/" role="button" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+      <!--github -->
+      <a class="btn btn-outline-light btn-floating m-1" style="color: white;" href="https://github.com/SurajShrestha0/Library-Management-System" role="button" target="_blank"><i class="fab fa-github"></i></a>
+    </section>
+    <!--Footer text -->
+    <section class="">
+      <p>&copy Library Management System|All rights reserved</p>
+    </section>
+  </div>
+</footer>
