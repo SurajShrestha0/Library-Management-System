5d7031b29a05c0a0d7831cf49d750de49e5b0ee5
Naresh Sejwal <107300202+Naresh132@users.noreply.github.com>
2023-05-08 13:10:53 +0545

    footer.php

---
+++ student/footer.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<!-- html -->
+<html>
+
+<head>
+	<title>Footer</title>
+	<!-- font-awesome link -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+</head>
+
+<body>
+	<!-- footer css -->
+	<footer class="bg-dark text-center text-black" style="position: fixed; bottom: 0; width: 100%; text-align: center; color: #333333;left: 60px; margin-bottom: 10px;">
+		<div class="container p-4">
+			<!--Social media link -->
+			<section class="mb-4">
+				<!--Facebook -->
+				<a class="btn btn-outline-light btn-floating m-1" style="color: white;" href="https://www.facebook.com/gops.bhattarai.7" role="button" target="_blank"><i class="fab fa-facebook-f"></i></a>
+				<!--Instgram -->
+				<a class="btn btn-outline-light btn-floating m-1" style="color: white;" href="https://www.instagram.com/sooraj__stha/" role="button" target="_blank"><i class="fab fa-instagram"></i></a>
+				<!--linkedin text -->
+				<a class="btn btn-outline-light btn-floating m-1" style="color: white;" href="https://www.linkedin.com/in/naresh-sejwal-381303239/" role="button" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+				<!--github -->
+				<a class="btn btn-outline-light btn-floating m-1" style="color: white;" href="https://github.com/SurajShrestha0/Library-Management-System" role="button" target="_blank"><i class="fab fa-github"></i></a>
+			</section>
+			<!--Footer text -->
+			<section class="copyright" style="margin-top: 10px;">
+				<p>&copy Library Management System|All rights reserved</p>
+			</section>
+		</div>
+	</footer>
+</body>
+
+</html>
