8e9d137d6dd7c2ec461aa11164d84cb6c91f7649
Naresh Sejwal <107300202+Naresh132@users.noreply.github.com>
2023-05-08 16:26:01 +0545

    update book.php

--- admin/book.php
+++ admin/book.php
@@ -5,38 +5,39 @@ require('dbconn.php');
 <?php
 //checks whether the $_SESSION['UserId'] variable is set 
 if ($_SESSION['UserId']) {
-    ?>
-<!-- Html  declaration --> 
-<!DOCTYPE html>
-<html lang="en">
+?>
+    <!-- Html  declaration -->
+    <!DOCTYPE html>
+    <html lang="en">
 
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!--tag specifies the title of the page-->
         <title>LMS</title>
-         <!--tags that reference external CSS files-->
+        <!--tags that reference external CSS files-->
         <!--CSS files are from the Bootstrap framework-->
         <link type="text/css" href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
         <link type="text/css" href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
         <link type="text/css" href="css/theme.css" rel="stylesheet">
         <link type="text/css" href="images/icons/css/font-awesome.css" rel="stylesheet">
-        <link type="text/css" href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600'
-            rel='stylesheet'>
+        <link type="text/css" href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600' rel='stylesheet'>
     </head>
     <!--Body-->
+
     <body>
-          <!-- class for css -->
+        <!-- class for css -->
         <div class="navbar navbar-fixed-top">
             <div class="navbar-inner">
                 <div class="container">
                     <a class="btn btn-navbar" data-toggle="collapse" data-target=".navbar-inverse-collapse">
-                        <i class="icon-reorder shaded"></i></a><a class="brand" href="index.php">LMS </a>
+                        <i class="icon-reorder shaded"></i></a><a class="brand" href="home.
+                        php"><img src="images/nn.png" class="logo" /> LMS </a>
                     <div class="nav-collapse collapse navbar-inverse-collapse">
                         <ul class="nav pull-right">
                             <li class="nav-user dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                <img src="images/user.png" class="nav-avatar" />
-                                <b class="caret"></b></a>
+                                    <img src="images/user.png" class="nav-avatar" />
+                                    <b class="caret"></b></a>
                                 <ul class="dropdown-menu">
                                     <li><a href="index.php">Your Profile</a></li>
                                     <li class="divider"></li>
@@ -57,9 +58,9 @@ if ($_SESSION['UserId']) {
                     <div class="span3">
                         <div class="sidebar">
                             <ul class="widget widget-menu unstyled">
-                                <li class="active"><a href="index.php"><i class="menu-icon icon-home"></i>Home
-                                </a></li>
-                                 <li><a href="message.php"><i class="menu-icon icon-inbox"></i>Messages</a>
+                                <li class="active"><a href="home.php"><i class="menu-icon icon-home"></i>Dashboard
+                                    </a></li>
+                                <li><a href="message.php"><i class="menu-icon icon-inbox"></i>Messages</a>
                                 </li>
                                 <li><a href="student.php"><i class="menu-icon icon-user"></i>Manage Students </a>
                                 </li>
@@ -77,98 +78,102 @@ if ($_SESSION['UserId']) {
                     </div>
 
                     <div class="span9">
-                  <form class="form-horizontal row-fluid" action="book.php" method="post">
-                                        <div class="control-group">
-                                            <label class="control-label" for="Search"><b>Search:</b></label>
-                                            <div class="controls">
-                                                <input type="text" id="title" name="title" placeholder="Enter Name/ID of Book" class="span8" required>
-                                                <button type="submit" name="submit"class="btn">Search</button>
-                                            </div>
-                                        </div>
-                                    </form>
-                                    <br>
+                        <form class="form-horizontal row-fluid" action="book.php" method="post">
+                            <div class="control-group">
+                                <label class="control-label" for="Search"><b>Search:</b></label>
+                                <div class="controls">
+                                    <input type="text" id="title" name="title" placeholder="Enter Name/ID of Book" class="span8" required>
+                                    <!-- Searching-->
+                                    <button type="submit" name="submit" class="btn">Search</button>
+                                </div>
+                            </div><!-- end -->
+                        </form>
+                        <br>
+                        <?php
+                        // Check if the form has been submitted
+                        if (isset($_POST['submit']))
+                        // Retrieve the title from the submitted form data
+                        {
+                            $s = $_POST['title'];
+                            // Construct a SQL query to retrieve data from the 'book' table
+                            // based on the submitted title or BookId
+                            $sql = "select * from LMS.book where BookId='$s' or Title like '%$s%'";
+                        } else
+                            $sql = "select * from LMS.book";
+
+                        $result = $conn->query($sql);
+                        $rowcount = mysqli_num_rows($result);
+
+                        if (!($rowcount))
+                            echo "<br><center><h2><b><i>No Results</i></b></h2></center>";
+                        else {
+
+
+                        ?>
+                            <table class="table" id="tables">
+                                <thead>
+                                    <tr>
+                                        <th>Book id</th>
+                                        <th>Book name</th>
+                                        <th>Availability</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
                                     <?php
-                                    if(isset($_POST['submit']))
-                                        {$s=$_POST['title'];
-                                            $sql="select * from LMS.book where BookId='$s' or Title like '%$s%'";
-                                        }
-                                    else
-                                        $sql="select * from LMS.book";
 
-                                    $result=$conn->query($sql);
-                                    $rowcount=mysqli_num_rows($result);
+                                    //$result=$conn->query($sql);
+                                    while ($row = $result->fetch_assoc()) {
+                                        $bookid = $row['BookId'];
+                                        $name = $row['Title'];
+                                        $avail = $row['Availability'];
 
-                                    if(!($rowcount))
-                                        echo "<br><center><h2><b><i>No Results</i></b></h2></center>";
-                                    else
-                                    {
 
                                     ?>
-                        <table class="table" id = "tables">
-                                  <thead>
-                                    <tr>
-                                      <th>Book id</th>
-                                      <th>Book name</th>
-                                      <th>Availability</th>
-                                      <th></th>
-                                    </tr>
-                                  </thead>
-                                  <tbody>
-                                    <?php
-                 
-                            //$result=$conn->query($sql);
-                            while($row=$result->fetch_assoc())
-                            {
-                                $bookid=$row['BookId'];
-                                $name=$row['Title'];
-                                $avail=$row['Availability'];
-                            
-                           
-                            ?>
-                                    <tr>
-                                      <td><?php echo $bookid ?></td>
-                                      <td><?php echo $name ?></td>
-                                      <td><b><?php echo $avail ?></b></td>
-                                        <td><center>
-                                            <a href="bookdetails.php?id=<?php echo $bookid; ?>" class="btn btn-primary">Details</a>
-                                            <a href="edit_book_details.php?id=<?php echo $bookid; ?>" class="btn btn-success">Edit</a>
-                                        </center></td>
-                                    </tr>
-                               <?php }} ?>
-                               </tbody>
-                                </table>
-                            </div>
+                                        <tr>
+                                            <td><?php echo $bookid ?></td>
+                                            <td><?php echo $name ?></td>
+                                            <td><b><?php echo $avail ?></b></td>
+                                            <td>
+                                                <center>
+                                                    <a href="bookdetails.php?id=<?php echo $bookid; ?>" class="btn btn-primary">Details</a>
+                                                    <a href="edit_book_details.php?id=<?php echo $bookid; ?>" class="btn btn-success">Edit</a>
+                                                </center>
+                                            </td>
+                                        </tr>
+                                <?php }
+                                } ?>
+                                </tbody>
+                            </table>
+                    </div>
                     <!--/.span9-->
                 </div>
             </div>
             <!--/.container-->
         </div>
-<div class="footer">
-            <div class="container">
-                <b class="copyright">&copy; 2018 Library Management System </b>All rights reserved.
-            </div>
-        </div>
-        
+        <!--Footer  -->
+        <?php include('footer.php') ?>
+
         <!--/.wrapper-->
         <script src="scripts/jquery-1.9.1.min.js" type="text/javascript"></script>
-<!-- jQuery library version 1.9.1 -->
-<script src="scripts/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>
-<!-- jQuery UI library version 1.10.1 -->
-<script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-<!-- Bootstrap library version 3.0.0 -->
-<script src="scripts/flot/jquery.flot.js" type="text/javascript"></script>
-<!-- Flot library version 0.8.1 -->
-<script src="scripts/flot/jquery.flot.resize.js" type="text/javascript"></script>
-<!-- Flot resize library -->
-<script src="scripts/datatables/jquery.dataTables.js" type="text/javascript"></script>
-<!-- DataTables library version 1.10.0 -->
-<script src="scripts/common.js" type="text/javascript"></script>
-<!-- Custom JavaScript code -->
-      
+        <!-- jQuery library version 1.9.1 -->
+        <script src="scripts/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>
+        <!-- jQuery UI library version 1.10.1 -->
+        <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+        <!-- Bootstrap library version 3.0.0 -->
+        <script src="scripts/flot/jquery.flot.js" type="text/javascript"></script>
+        <!-- Flot library version 0.8.1 -->
+        <script src="scripts/flot/jquery.flot.resize.js" type="text/javascript"></script>
+        <!-- Flot resize library -->
+        <script src="scripts/datatables/jquery.dataTables.js" type="text/javascript"></script>
+        <!-- DataTables library version 1.10.0 -->
+        <script src="scripts/common.js" type="text/javascript"></script>
+        <!-- Custom JavaScript code -->
+
     </body>
-</html>
-<?php }
-else {
+
+    </html>
+<?php } else {
     //Display aleart box message
     echo "<script type='text/javascript'>alert('Access Denied!!!')</script>";
 } ?>
