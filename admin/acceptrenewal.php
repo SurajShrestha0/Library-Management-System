58e06e5848ac5acc7bae353329824a5d8f82dea7
Naresh Sejwal <107300202+Naresh132@users.noreply.github.com>
2023-05-08 16:24:06 +0545

    Updated

--- admin/acceptrenewal.php
+++ admin/acceptrenewal.php
@@ -6,22 +6,24 @@ $bookid = $_GET['id1'];
 $bookid=$_GET['id1'];
 $rollno=$_GET['id2'];
 // Retrieve the user's category from the database
-$sql="select Category from LMS.user where RollNo='$rollno'";
+$sql="select Category from LMS.user where UserId='$rollno'";
 $result=$conn->query($sql);
 $row=$result->fetch_assoc();
 $category=$row['Category'];
-// Update the due date and renewals left for the book record based on the user's category
-if($category == 'GEN' || $category == 'OBC' )
-{$sql1="update LMS.record set Due_Date=date_add(Due_Date,interval 60 day),Renewals_left=0 where BookId='$bookid' and RollNo='$rollno'";
+//Extract the value of the "Category" column from the fetched row and assign it to a variable named $category
+$category=$row['Category'];
+if($category == 'BSc' || $category == 'BIBM' )
+//update record id, rollno
+{$sql1="update LMS.record set Date_of_Issue=D,Due_Date=D1,Renewals_left=1 where BookId='$bookid' and UserId='$rollno'";
  
 if($conn->query($sql1) === TRUE)
 {
  // Delete the renewal request from the database
- $sql3="delete from LMS.renew where BookId='$bookid' and RollNo='$rollno'";
+ $sql3="delete from LMS.renew where BookId='$bookid' and UserId='$rollno'";
  // Execute the SQL query using the connection object and store the result
  $result=$conn->query($sql3);
  //sql query for insert the value in data
- $sql5="insert into LMS.message (RollNo,Msg,Date,Time) values ('$rollno','Your request for renewal of BookId: $bookid  has been accepted',curdate(),curtime())";
+ $sql5="insert into LMS.message (UserId,Msg,Date,Time) values ('$rollno','Your request for renewal of BookId: $bookid  has been accepted',curdate(),curtime())";
 //Execute the SQL query
  $result=$conn->query($sql5);
  // Display the error message
@@ -29,7 +31,7 @@ echo "<script type='text/javascript'>alert('Success')</script>";
 //refresh page after 0.01 seconds and redirect to renew_requests.php         
 header( "Refresh:0.01; url=renew_requests.php", true, 303);
 }
-else
+else//pritn error message
 {
 	echo "<script type='text/javascript'>alert('Error')</script>";
     header( "Refresh:0.01; url=renew_requests.php", true, 303);
@@ -37,15 +39,15 @@ else
 }
 else
 //sql query for update record database table
-{$sql2="update LMS.record set Due_Date=date_add(Due_Date,interval 180 day),Renewals_left=0 where BookId='$bookid' and RollNo='$rollno'";
+{$sql2="update LMS.record set Due_Date=date_add(Due_Date,interval 180 day),Renewals_left=0 where BookId='$bookid' and UserId='$rollno'";
 //Execute the SQL query and check if it was successful
 if($conn->query($sql2) === TRUE)
 //SQL query to delete record from database table
-{$sql4="delete from LMS.renew where BookId='$bookid' and RollNo='$rollno'";
+{$sql4="delete from LMS.renew where BookId='$bookid' and UserId='$rollno'";
 // Execute the SQL query using the connection object and store the result
  $result=$conn->query($sql4);
 //insert the data value into message databse table
- $sql6="insert into LMS.message (RollNo,Msg,Date,Time) values ('$rollno','Your request for renewal of BookId: $bookid has been accepted',curdate(),curtime())";
+ $sql6="insert into LMS.message (UserId,Msg,Date,Time) values ('$rollno','Your request for renewal of BookId: $bookid has been accepted',curdate(),curtime())";
 // Execute the SQL query using the connection object and store the result
  $result=$conn->query($sql6);
  //display the  success message 
